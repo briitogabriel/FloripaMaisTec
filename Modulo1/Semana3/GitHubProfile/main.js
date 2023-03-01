33bb54f9c724e3dcb632ece059649776eb443803
briitogabriel <gabriel.brito.a@hotmail.com>
2023-02-28 22:43:02 -0300

    RickAndMorty in construction

--- Modulo1/Semana3/GitHubProfile/main.js
+++ Modulo1/Semana3/GitHubProfile/main.js
@@ -29,38 +29,3 @@ const apiData = () =>{
 }
 
 window.onload = apiData();
-
-// {
-//   "login": "briitogabriel",
-//   "id": 71029907,
-//   "node_id": "MDQ6VXNlcjcxMDI5OTA3",
-//   "avatar_url": "https://avatars.githubusercontent.com/u/71029907?v=4",
-//   "gravatar_id": "",
-//   "url": "https://api.github.com/users/briitogabriel",
-//   "html_url": "https://github.com/briitogabriel",
-//   "followers_url": "https://api.github.com/users/briitogabriel/followers",
-//   "following_url": "https://api.github.com/users/briitogabriel/following{/other_user}",
-//   "gists_url": "https://api.github.com/users/briitogabriel/gists{/gist_id}",
-//   "starred_url": "https://api.github.com/users/briitogabriel/starred{/owner}{/repo}",
-//   "subscriptions_url": "https://api.github.com/users/briitogabriel/subscriptions",
-//   "organizations_url": "https://api.github.com/users/briitogabriel/orgs",
-//   "repos_url": "https://api.github.com/users/briitogabriel/repos",
-//   "events_url": "https://api.github.com/users/briitogabriel/events{/privacy}",
-//   "received_events_url": "https://api.github.com/users/briitogabriel/received_events",
-//   "type": "User",
-//   "site_admin": false,
-//   "name": "Gabriel Brito Araújo",
-//   "company": null,
-//   "blog": "https://www.linkedin.com/in/brittogabriel/",
-//   "location": "Florianópolis",
-//   "email": null,
-//   "hireable": true,
-//   "bio": "Developer and Civil Engineer. Curious and directional thinking, I like to contribute to a nice and productive environment with my abilities.",
-//   "twitter_username": null,
-//   "public_repos": 20,
-//   "public_gists": 0,
-//   "followers": 8,
-//   "following": 3,
-//   "created_at": "2020-09-09T16:11:21Z",
-//   "updated_at": "2022-10-18T14:02:42Z"
-// }
