a3c1b3fba7c851ae64f7723a44766d69024fa6a7
briitogabriel <gabriel.brito.a@hotmail.com>
2023-03-02 23:05:30 -0300

    TechBakeryMegaPlus v1

--- Modulo1/Semana4/TechBakeryMegaPlus/functions.js
+++ Modulo1/Semana4/TechBakeryMegaPlus/functions.js
@@ -47,15 +47,14 @@ class CashRegister {
     return this.#products.find((product) => product.ccode === ccode);
   }
 
-  totalShopping() {
+  getTotalShopping() {
     return this.#itemsRegistered.reduce((totalShop, item) => totalShop + (item.product.price * item.qtd), 0)
   }
 
   closeShopping(amount) {
     this.#amount = amount;    //number
-    const totalShop = this.totalShopping();
+    const totalShop = this.getTotalShopping();
     this.#change = this.#amount - totalShop;
-    return this.#change;
   }
   get amount() {
     return this.#amount;
@@ -75,21 +74,57 @@ class CashRegister {
 
 const register = new CashRegister();
 
+const registerProducts = () => {
+  let option = true;
+  do {
+    let newName = prompt(`Enter the Product name:`);
+    let newtPrice = Number(prompt(`Enter the price:`));
+    let newCcode = Number(prompt(`Enter the codebar:`));
+
+    register.addProduct(newCcode, newtPrice, newName)
+    option = confirm(`Do you want to add a new product?`)
+  } while (option !== false);
+
+  alert('Check the console (F12) to see the list of products registered.')
+  console.log(register.products)
+}
+
 const startShopping = () => {
 
+  // amount = Number(prompt(`Your total shopping for ${productList.length} products is R$${totalShop}. Would you like a change for how much?`));
+
+
+  // do {
+  //   if (isNaN(amount)) {
+  //     alert('Please provide a valid amount!');
+  //     amount = Number(prompt(`Your total shopping is R$${totalShop}. Would you like a change for how much?`));
+  //   } else if (amount < totalShop) {
+  //     alert(`R$${parseFloat(amount).toFixed(2)} Is not enought! You must pay at least your total shopping amount (R$${parseFloat(totalShop).toFixed(2)})`);
+  //     amount = Number(prompt(`Your total shopping is R$${totalShop}. Would you like a change for how much?`));
+  //   }
+  // } while (amount < totalShop || isNaN(amount));
+
+  // change = parseFloat(amount - totalShop).toFixed(2);
+
   register.welcome(prompt("What is the customer's name?"));
-
-  register.addProduct(001, 10, 'Produto 1')
-  register.addProduct(002, 20, 'Produto 2')
-
-  const productFound = register.findProduct(002);
   
-  if (productFound == undefined) {
-    console.log('Product not found')
-  } else {
-    register.addItem(productFound, 3)
-    register.addItem(productFound, 7)
-  }
+  let option = true;
+  do {
+    const productFound = register.findProduct(
+      Number(prompt(`Enter the codebar of the product that ${register.client} wants to buy:`))
+    );
+    
+    if (productFound == undefined) {
+      alert('Product not found!')
+    } else {
+      
+      const qtToBuy = Number(prompt(`How many '${productFound.name}s' ${register.client} wants to buy?`))
+      register.addItem(productFound, qtToBuy)
+    }
+    option = confirm(`Does ${register.client} wants to add new products?`)
+  } while (option !== false);
+
+  alert('Added to Cart.')
 
   // productList.forEach((item, index) => {
   //   let li = document.createElement('li');
@@ -112,16 +147,35 @@ const startShopping = () => {
 
 const handleClose = () => {
 
-  console.log(register)
-  console.log('Total Shop: ' + register.totalShopping())
-  console.log('Change: ' + register.closeShopping(500))
+  let totalShop = register.getTotalShopping()
+  let amountPaid = prompt(`The shopping total cost is R$${parseFloat(totalShop).toFixed(2)}\nHow much ${register.client} would like the change for?`)
+  register.closeShopping(amountPaid)
+  let totalChange = register.change
+  let itemsList = register.itemsRegistered
 
   shopTable.innerHTML = `
     <li>
-      <b>Tech Bakery - Shopping Report:</b>
+      <b>Tech Bakery - ${register.client}'s Shopping Report:</b>
     </li>
-    `;
+    
+    ${itemsList.map((item) => (
+      `<li>
+        Product: ${item.product.name} -> R$${parseFloat(item.product.price).toFixed(2)} x ${item.qtd} = R$${parseFloat(item.product.price * item.qtd).toFixed(2)}
+      </li>`
+    ))}
+
+    
+    <li>
+      <b>Total shopping: R$${parseFloat(totalShop).toFixed(2)}</b>
+    </li>
+
+    <li>
+      Amount paid: R$${parseFloat(amountPaid).toFixed(2)}
+    </li>
+
+    <li>
+      Change: R$${parseFloat(totalChange).toFixed(2)}
+    </li>`;
 
   register.resetShop();
-  console.log(register)
 }
