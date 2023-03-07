a9a4ec22e5670b9339c792defd2d542c5c6b10f3
briitogabriel <gabriel.brito.a@hotmail.com>
2023-03-07 20:27:00 -0300

    M01.S05.Ex02 - ordering arrays

--- Modulo1/Semana5/server.js
+++ Modulo1/Semana5/server.js
@@ -1,6 +1,6 @@
 // const path = require('path')      //OUTRA MANEIRA DE IMPORTAR BIBLIOTECAS NODE: import path from 'path'
 
-console.log(`------------- Exercise 01 --------------`);
+console.log(`\n------------- Exercise 01 - DELETE DUPLICATED --------------`);
 
 const list = ['Pedro', 'José', 'Aderbal', 'Danilo', 'Luisa', 'Vitoria', 'Luis', 'Danilo', 'José'];
 const distinctList = [];
@@ -16,3 +16,21 @@ list.forEach(item => {
 
 console.log(`Original Array: [${list}]`);
 console.log(`Duplicated: ${duplicate}\nDistinct Array: [${distinctList}]`);
+
+
+
+console.log(`\n------------- Exercise 02 - ARRAY ORDERING --------------`);
+
+const users = ['Pedro', 'José', 'Aderbal', 'Danilo', 'Luisa', 'Vitoria'];
+const fruits = ['Banana', 'Morango', 'Maçã', 'Uva', 'Pêra' , 'Laranja'];
+const orderedArray = [];
+
+// Expected: ['Pedro - Laranja', ‘José - Pêra’, 'Aderbal - Uva', 'Danilo - Maçã', 'Luisa - Morango', 'Vitoria - Banana' ]
+
+for (let i = 0; i < users.length; i++) {
+  orderedArray.push(` ${users[i]} - ${fruits[users.length - (i+1)]}`);  
+}
+
+console.log(`Users Array: [${users}]`);
+console.log(`Fruits Array: [${fruits}]`);
+console.log(`Ordered combined Array: [${orderedArray}]`);
