d562e767798c87afff79009615bb2c172d7fa2c2
briitogabriel <gabriel.brito.a@hotmail.com>
2023-03-07 20:15:20 -0300

    M01.S05.Ex01 - delete duplicated

--- Modulo1/Semana5/server.js
+++ Modulo1/Semana5/server.js
@@ -1,1 +1,18 @@
 // const path = require('path')      //OUTRA MANEIRA DE IMPORTAR BIBLIOTECAS NODE: import path from 'path'
+
+console.log(`------------- Exercise 01 --------------`);
+
+const list = ['Pedro', 'José', 'Aderbal', 'Danilo', 'Luisa', 'Vitoria', 'Luis', 'Danilo', 'José'];
+const distinctList = [];
+let duplicate = '';
+
+list.forEach(item => {
+  if(distinctList.includes(item)) {
+    duplicate = duplicate + `${item}, `;
+  } else {
+    distinctList.push(item)
+  }
+})
+
+console.log(`Original Array: [${list}]`);
+console.log(`Duplicated: ${duplicate}\nDistinct Array: [${distinctList}]`);
