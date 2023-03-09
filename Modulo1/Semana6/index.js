ee38a6ce0dbc8a6adef1094ab163a591d5a01b85
briitogabriel <gabriel.brito.a@hotmail.com>
2023-03-08 23:22:35 -0300

    API route with a name URL param

--- Modulo1/Semana6/index.js
+++ Modulo1/Semana6/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+
+// ------- EXERCISE 01 - SEND A SIMPLES MESSAGE --------
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello there!')
+});
+
+// ------- EXERCISE 02 - SEND A MESSAGE USING URL PARAMS --------
+
+app.get('/nome=:name', (req, res) => {
+  let name = req.params.name;
+  res.send(`API route created by ${name}.`)
 });
 
 app.listen(3333);
