a23ded4c68d007ce6f3f8ea89f25c293b6e31258
briitogabriel <gabriel.brito.a@hotmail.com>
2023-03-09 21:05:03 -0300

    Exercise 5 - JSON through body

--- Modulo1/Semana6/index.js
+++ Modulo1/Semana6/index.js
@@ -10,11 +10,18 @@ app.get('/', (req, res) => {
   res.send('Hello there!')
 });
 
-// ------- EXERCISE 02 - SEND A MESSAGE USING URL PARAMS --------
+// ------- EXERCISE 02 - GET A MESSAGE USING URL PARAMS --------
 
 app.get('/nome=:name', (req, res) => {
   let name = req.params.name;
   res.send(`API route created by ${name}.`)
+});
+
+// ------- EXERCISE 03 - SEND A MESSAGE THROUGH REQ BODY AND RETURN JSON --------
+
+app.post('/bodymessage', (req, res) => {
+  let message = req.body;
+  res.send(JSON.stringify(message))
 });
 
 app.listen(3333);
