3c584bf55d380f7244d20fd519e04504a95ef650
briitogabriel <gabriel.brito.a@hotmail.com>
2023-03-12 15:52:18 -0300

    API v1 - routes: addUser and deleteUser

--- Modulo1/Semana6/Ex6to8/src/controllers/controller.js
+++ Modulo1/Semana6/Ex6to8/src/controllers/controller.js
@@ -13,32 +13,61 @@ const getUserId = (req, res) => {
   const userFound = users.find(user => (
     user.id === idRequest
   ));
-  res.status(200).json(userFound);
+  
+  if(userFound == undefined) {
+    res.status(400).json({message: `User ID not found.`});
+  } else {
+    res.status(200).json(userFound);
+  }
 }
 
 const addUser = (req, res) => {
   const newId = maxId+1;
   const newName = req.body.name;
 
-  users.push({
-    "id": newId,
-    "name": newName
-  });
+  if(newName.length > 0) {
+    users.push({
+      "id": newId,
+      "name": newName
+    });
+  
+    res.status(200).json({message: `User ${newName} created with success!`});
+  
+    // ---> COULD NOT FIX THIS ISSUE: Error: ENOENT: no such file or directory, open 'D:\\Tecnologia\\CURSOS\\Floripa_MaisTEC\\Modulo1\\data\\users.json'
+  
+    // fs.writeFile("../../data/users.json", JSON.stringify(users), (err) => {
+    //   if (err) {
+    //     res.status(400).json({message: `An error occured while writing JSON Object to File: ${err}`});
+    //   } else {
+    //     res.status(200).json({message: `User ${newName} created with success!`});
+    //   }
+    // });
+    maxId = newId;
+  } else {
+    res.status(400).json({message: `Please inform a valid user name.`});
+  }
+}
 
-  res.status(200).json({message: `User ${newName} created with success!`});
+const deleteUser = (req, res) => {
+  const idRequest = parseInt(req.body.id);
+  const userFound = users.find(user => (
+    user.id === idRequest
+  ));
 
-  // fs.writeFile("../../data/users.json", JSON.stringify(users), 'utf8', (err) => {
-  //   if (err) {
-  //     res.status(400).json({message: `An error occured while writing JSON Object to File: ${err}`});
-  //   } else {
-  //     res.status(200).json({message: "User created with success!"});
-  //   }
-  // });
-  maxId = newId;
+  let deleteIndex = users.indexOf(userFound);
+  
+  if(deleteIndex > -1) {
+    const deletedName = userFound.name;
+    users.splice(deleteIndex, 1);
+    res.status(200).json({message: `User ${deletedName} deleted.`});
+  } else {
+    res.status(400).json({message: `User ID not found.`});
+  }
 }
 
 module.exports = {
   getUserId,
   getAllUsers,
-  addUser
+  addUser,
+  deleteUser
 }
--- Modulo1/Semana6/Ex6to8/src/routes/router.js
+++ Modulo1/Semana6/Ex6to8/src/routes/router.js
@@ -6,6 +6,7 @@ const routerController = require(`../controllers/controller`)
 router.get('/get-all-users/', routerController.getAllUsers);
 router.get('/get-user-id/:id', routerController.getUserId);
 router.post('/add-user/', routerController.addUser);
+router.post('/delete-user/', routerController.deleteUser);
 
 
 module.exports = router;
