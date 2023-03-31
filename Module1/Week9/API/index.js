48667be61182ab1e291bf11abac7d2de07d9f34d
briitogabriel <gabriel.brito.a@hotmail.com>
2023-03-30 21:36:44 -0300

    handle exceptions

--- Module1/Week9/API/index.js
+++ Module1/Week9/API/index.js
@@ -12,30 +12,49 @@ connection.sync();
 console.log('Connection established.');
 
 app.get('/', (_, res) => {
-  res.json({
-    message: 'Welcome!'
-  })
+  try {
+    res.json({
+      message: 'Welcome!'
+    })
+    
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({error: 'Could not process your request'});
+  }
 });
 
 
 app.post('/tasks', async (req, res) => {
-  const newTask = {
-    name: req.body.name,
-    description: req.body.description
-  };
-
-  if (!newTask.name) {
-    return res.status(400).json({error: "Name is mandatory."});
-  }
-
-  const postTask = await Task.create(newTask);
-  res.status(201).json(postTask);
+  try {
+    const newTask = {
+      name: req.body.name,
+      description: req.body.description
+    };
   
-})
+    if (!newTask.name) {
+      return res.status(400).json({error: "Name is mandatory."});
+    // } else if (VALIDAR SE O NOME JÁ EXISTE) {
+    //   return
+    }
+  
+    const postTask = await Task.create(newTask);
+    res.status(201).json(postTask);
+    
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({error: 'Could not process your request'});
+  }
+});
 
 app.get('/tasks', async (_, res) => {
-  const getTasks = await Task.findAll();
-  res.status(200).json(getTasks);
-})
+  try {
+    const getTasks = await Task.findAll();
+    res.status(200).json(getTasks);
+    
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({error: 'Could not process your request'});
+  }
+});
 
 app.listen(3000, () => console.log('App listening on port 3000'))
